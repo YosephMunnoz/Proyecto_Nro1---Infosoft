7c55d99c10528bdb6373788377a6d66a515a9478
YosephMunnoz <130986694+YosephMunnoz@users.noreply.github.com>
2023-07-13 22:21:29 -0400

    update

--- Inventario/servidorVirtual.php
+++ Inventario/servidorVirtual.php
@@ -22,19 +22,19 @@
                     <div class="container-fluid">
                         <div class="row mb-3">
                             <div class="col-6 d-flex justify-content-between">
-                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales en&servidor=N/A&value=2&entorno=PRODUCCION&tipo=SERVIDOR" class="d-none d-sm-inline-block btn btn-danger btn-sm text-white">
+                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales&servidor=N/A&value=2&entorno=PRODUCCION&tipo=SERVIDOR" class="d-none d-sm-inline-block btn btn-danger btn-sm text-white">
                                     <i class="fas fa-cogs fa-lg text-white"></i> Producción
                                 </a>
-                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales en&servidor=N/A&value=2&entorno=TESTING&tipo=SERVIDOR" class="d-none d-sm-inline-block btn btn-danger btn-sm text-white">
+                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales&servidor=N/A&value=2&entorno=TESTING&tipo=SERVIDOR" class="d-none d-sm-inline-block btn btn-danger btn-sm text-white">
                                 <i class="fa-solid fa-cubes fa-lg text-white"></i> Testing
                                 </a>
-                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales en&servidor=N/A&value=2&entorno=DESARROLLO&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
+                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales&servidor=N/A&value=2&entorno=DESARROLLO&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
                                 <i class="fa-solid fa-code fa-lg text-white"></i> Desarrollo
                                 </a>
-                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales en&servidor=N/A&value=2&entorno=IMPLEMENTACION&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
+                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales&servidor=N/A&value=2&entorno=IMPLEMENTACION&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
                                 <i class="fa-solid fa-check-double fa-lg text-white"></i> Implementación
                                 </a>
-                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales en&servidor=N/A&value=2&entorno=APAGADO&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
+                                <a href="../../Infosoft3/Inventario/servidorVirtual.php?nombre=Servidores Virtuales&servidor=N/A&value=2&entorno=APAGADO&tipo=SERVIDOR" class="d-none d-sm-inline-block  btn btn-danger btn-sm text-white">
                                 <i class="fa-solid fa-power-off fa-lg text-white"></i> Apagados
                                 </a>
                             </div>
